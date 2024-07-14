4379860b06b867ff9c97ac85a796608e6860c3c0
ParvinAhmadov <pervin.akhmedov.20@gmail.com>
2024-07-14 19:23:36 +0400

    creat card

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,102 @@
 
-// windows toTop
-const toTop = document.querySelector("#to-top");
+let BASE_URL = "http://localhost:5000";
+const cardSwiper = document.querySelector("#swiper-card");
 
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    toTop.style.display = "flex";
-  } else {
-    toTop.style.display = "none";
-
+const getApiData = async (endPoint, cb) => {
+  try {
+    let response = await fetch(`${BASE_URL}/${endPoint}`);
+    let data = await response.json();
+    cb(data);
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
-})
+};
+
+const deleteApiData = async (endPoint, id, cb,...props) => {
+  let response = await fetch(`${BASE_URL}/${endPoint}/${id}`, {
+    method: "DELETE",
+    ...props
+  });
+  cb(response);
+};
 
 
-toTop.addEventListener("click", () => {
-  window.scrollTo(0, 0);
+const postApiData = async (endPoint, data) => {
+  try {
+    let response = await fetch(`${BASE_URL}/${endPoint}`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error posting data:", error);
+  }
+};
+// getApidata products 
+getApiData("data", (data) => {
+  data.map((item) => {
+    cardSwiper.innerHTML += `
+    <div class="swiper-slide">
+    <div class="products-swipper-btn">
+        <button><i
+                class="fa-solid fa-magnifying-glass"></i></button>
+            <button>   <i class="fa-regular fa-heart"></i>
+            </button>
+                <button products-delete-btn >Delete</button>
+    </div>
+    <div class="products-img">
+        <img
+            src="${item.image}"
+            alt />
+
+    </div>
+    <div class="products-content">
+        <h3>${item.name}</h3>
+        <p>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+        </p>
+        <span>$${item.price}</span>
+        <button class="products-content-btn">
+            <span>Select options</span>
+        </button>
+    </div>
+
+</div>
+    `;
+  });
 });
 
 
-new Swiper(".productsSwiper", {
-    slidesPerView: 4,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-});
+const createBTN = document.querySelector("#create_product");
+const swipperImage = document.querySelector("#image");
+const swippername = document.querySelector("#name");
+const swipperprice = document.querySelector("#price");
 
-
-
-
-new Swiper(".menuSwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-
-});
-
-
+createBTN &&
+  createBTN.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const usData = {
+      image: swipperImage.value,
+      name: swippername.value,
+      price: swipperprice.value,
+    };
+    const postData = await postApiData("data", usData);
+    console.log("Post response:", postData);
+  });
   
+
+// deleteApiData products
+
+document.addEventListener("click", (e) => {
+  if (e.target && e.target.classList.contains("delete-btn")) {
+    const cardId = e.target.dataset.id;
+    deleteCard(cardId);
+  }
+});
---
+++ src/scripts/swiper.js
@@ -0,0 +1,37 @@
+
+// windows toTop
+const toTop = document.querySelector("#to-top");
+
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    toTop.style.display = "flex";
+  } else {
+    toTop.style.display = "none";
+
+  }
+})
+
+
+toTop.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
+
+new Swiper(".productsSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
+
+new Swiper(".menuSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+
+});
